2e798df08885f36ba72c1f42c2d663b4943e2abe
James Munns <james@onevariable.com>
2022-06-21 21:48:54 +0200

    simple test

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ fn main() {
     println!("Hello, world!");
 }
 
-use std::marker::PhantomData;
 use std::ops::DerefMut;
 
 use postcard::ser_flavors::Flavor as SerFlavor;
-use postcard::de_flavors::Flavor as DeFlavor;
 use postcard::Result as PCResult;
+use serde::{Serialize, Deserialize};
 
 pub struct LenFramedSer<F> {
     inner: F,
@@ -51,43 +50,112 @@ where
     }
 }
 
-pub struct LenFramedDe<'de, F>
-where
-    F: DeFlavor<'de> + 'de
-{
-    inner: F,
-    _plt: PhantomData<&'de ()>,
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Outer {
+    foo: (u8, u32),
+    bar: Vec<String>,
+    baz: Vec<Inner>,
 }
 
-impl<'de, F> LenFramedDe<'de, F>
-where
-    F: DeFlavor<'de>
-{
-    pub fn new(mut flav: F) -> PCResult<(usize, Self)> {
-        let mut buf = [0u8; 4];
-        buf.copy_from_slice(flav.try_take_n(4)?);
-        let len = u32::from_le_bytes(buf) as usize;
-        let me = Self { inner: flav, _plt: PhantomData };
-        Ok((len, me))
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Inner {
+    a: u32,
+    b: u64,
+    c: Vec<u8>,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use postcard::serialize_with_flavor;
+    use postcard::ser_flavors::StdVec;
+    use postcard::de_flavors::Slice;
+
+    #[test]
+    fn smoke() {
+        let data = Outer {
+            foo: (1, 2),
+            bar: vec!["Hello".into(), "World!".into()],
+            baz: vec![
+                Inner { a: 3, b: 4, c: vec![5, 6, 7] },
+                Inner { a: 8, b: 9, c: vec![0, 1, 2] },
+                Inner { a: 0, b: 1, c: vec![2, 3, 4] },
+            ],
+        };
+
+        let serdata = serialize_with_flavor::<Outer, LenFramedSer<StdVec>, Vec<u8>>(
+            &data,
+            LenFramedSer::new(StdVec::new()).unwrap(),
+        ).unwrap();
+
+        println!("{}, {:?}", serdata.len(), serdata);
+
+        // Sender task psuedocode:
+        //
+        // tcp.send(&data).await?;
+
+        // ...
+
+        // Receiver task psuedocode (I don't know how async streams work...):
+        //
+        // loop {
+        //     let len = u32::from_le_bytes(tcp.recv(4).await) as usize;
+        //     let serdata = tcp.recv(len).await.to_vec();
+        //     yield the_code_below()?;
+        // }
+
+
+        let flav = Slice::new(&serdata[4..]);
+        let mut deser = postcard::Deserializer::from_flavor(flav);
+        let dedata = Outer::deserialize(&mut deser).unwrap();
+        assert_eq!(data, dedata);
     }
 }
 
-impl<'de, F> DeFlavor<'de> for LenFramedDe<'de, F>
-where
-    F: DeFlavor<'de>
-{
-    type Remainder = F::Remainder;
-    type Source = F;
 
-    fn pop(&mut self) -> PCResult<u8> {
-        self.inner.pop()
-    }
+// TODO: I thought I needed this, but I really don't. With this simple data, it's easier to
+// just await the len first
 
-    fn try_take_n(&mut self, ct: usize) -> PCResult<&'de [u8]> {
-        self.inner.try_take_n(ct)
-    }
+// use std::marker::PhantomData;
+// use postcard::de_flavors::Flavor as DeFlavor;
 
-    fn finalize(self) -> PCResult<Self::Remainder> {
-        self.inner.finalize()
-    }
-}
+// pub struct LenFramedDe<'de, F>
+// where
+//     F: DeFlavor<'de> + 'de
+// {
+//     inner: F,
+//     _plt: PhantomData<&'de ()>,
+// }
+
+// impl<'de, F> LenFramedDe<'de, F>
+// where
+//     F: DeFlavor<'de>
+// {
+//     pub fn new(mut flav: F) -> PCResult<(usize, Self)> {
+//         let mut buf = [0u8; 4];
+//         buf.copy_from_slice(flav.try_take_n(4)?);
+//         let len = u32::from_le_bytes(buf) as usize;
+//         let me = Self { inner: flav, _plt: PhantomData };
+//         Ok((len, me))
+//     }
+// }
+
+// impl<'de, F> DeFlavor<'de> for LenFramedDe<'de, F>
+// where
+//     F: DeFlavor<'de>
+// {
+//     type Remainder = F::Remainder;
+//     type Source = F;
+
+//     fn pop(&mut self) -> PCResult<u8> {
+//         self.inner.pop()
+//     }
+
+//     fn try_take_n(&mut self, ct: usize) -> PCResult<&'de [u8]> {
+//         self.inner.try_take_n(ct)
+//     }
+
+//     fn finalize(self) -> PCResult<Self::Remainder> {
+//         self.inner.finalize()
+//     }
+// }
